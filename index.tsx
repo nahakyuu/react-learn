90df224e47e3745672bda6fdb1d1c0e052229c3d
aroa <root@aroa.me>
2017-06-06 15:42:48 +0800

    Specifying Attributes with JSX

--- index.tsx
+++ index.tsx
@@ -1,14 +1,16 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
 interface User {
     firstName: string
     lastName: string
+    avatarUrl: string
 }
 
 const user: User = {
     firstName: 'Harper',
-    lastName: 'Perez'
+    lastName: 'Perez',
+    avatarUrl: 'http://1.gravatar.com/avatar/6141af411a46e8732f95129f49be620b'
 }
 
 function formatName(user: User) {
@@ -17,12 +19,13 @@ function formatName(user: User) {
 
 function getGreeting(user?: User) {
     if (user) {
-        return <h1>Hello, {formatName(user)}!</h1>;
+        const element = <img src={user.avatarUrl} />
+        return <h1>{element}Hello, {formatName(user)}!</h1>
     }
-    return <h1>Hello, Stranger.</h1>;
+    return <h1>Hello, Stranger.</h1>
 }
 
 ReactDOM.render(
     Math.random() > 0.5 ? getGreeting(user) : getGreeting(),
     document.getElementById('root')
-);
+)

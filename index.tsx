c7878b78a560077450036eed0ff6b50e6307f305
aroa <root@aroa.me>
2017-06-06 15:33:48 +0800

    Embedding Expressions in JSX

--- index.tsx
+++ index.tsx
@@ -1,7 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+interface User {
+    firstName: string
+    lastName: string
+}
+
+const user: User = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+}
+
+function formatName(user: User) {
+    return `${user.firstName} ${user.lastName}`
+}
+
+const element =
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('root')
 );

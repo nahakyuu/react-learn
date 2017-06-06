b1921f8eca6d846bff55fa358e630cc440bc9b20
aroa <root@aroa.me>
2017-06-06 17:01:57 +0800

    Rendering a Component

--- index.tsx
+++ index.tsx
@@ -17,15 +17,12 @@ function formatName(user: User) {
     return `${user.firstName} ${user.lastName}`
 }
 
-function tick() {
-    const element =
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    )
+function Welcome(props: { name: string }) {
+    return <h1>Hello, {props.name}</h1>
 }
-setInterval(tick, 1000)
+
+const element = <Welcome name="aroa" />
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+)

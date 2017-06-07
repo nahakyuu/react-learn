6ed40317cbcc0502a4a3a7764d32fda305ccdccd
aroa <root@aroa.me>
2017-06-07 09:52:26 +0800

    Adding Local State to a Class

--- index.tsx
+++ index.tsx
@@ -62,22 +62,22 @@ const comment: CommentProp = {
     }
 }
 
-class Clock extends React.Component<{ date: Date }, null> {
+class Clock extends React.Component<{}, { date: Date }> {
+    constructor(props: {}) {
+        super(props)
+        this.state = { date: new Date() }
+    }
     render() {
         return (
             <div>
                 <h1>Hello, world!</h1>
-                <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         )
     }
 }
 
-function tick() {
-    ReactDOM.render(
-        <Clock date={new Date()} />,
-        document.getElementById('root')
-    )
-}
-
-setInterval(tick, 1000)
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+)
